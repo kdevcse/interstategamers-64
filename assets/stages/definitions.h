04e1598759fde05ff7941fc9813b69247eff7883
Kevin <klewiscse@gmail.com>
2019-12-03 01:58:06 -0600

    Setup Official Menu

--- assets/stages/definitions.h
+++ assets/stages/definitions.h
@@ -1,5 +1,18 @@
 #ifndef N64_STAGES_H
 #define N64_STAGES_H
+
+	/* Title img positions */
+	#define TITLE_X_POS   60
+	#define TITLE_Y_POS   70
+	
+	/* IG icon positions */
+	#define ICON_START_X_POS	70
+	#define ICON_START_Y_POS	190
+	
+	/* Default Background Color Values (Official IG Blue) */
+	#define DEF_BG_R	45
+	#define DEF_BG_G	50
+	#define DEF_BG_B	175
  
 	void stage00_init();
 	void stage00_update();
--- assets/stages/stage00.c
+++ assets/stages/stage00.c
@@ -1,31 +1,26 @@
 #include <nusys.h>
 #include "../../config.h"
 #include "../sprites/IGLogo32.h"
+#include "../sprites/IGMainWhite200.h"
 #include "definitions.h"
 
-static u8 color;
 static u8 r;
 static u8 g;
 static u8 b;
-static int x;
-static int y;
+static int pos_x;
+static int pos_y;
 static u8 debug;
+
+void DrawTitleImg(int x, int y);
 
 void DrawLogo(int x, int y);
 
 void ClearBackground(u8 r, u8 g, u8 b);
 
-void SwitchColor();
-
 void stage00_init(void)
 {
 	debug = 0;
-	color = 0;
-	r = 0;
-	g = 0;
-	b = 255;
-	x = 160;
-	y = 120;
+	pos_x = ICON_START_X_POS, pos_y = ICON_START_Y_POS;
 	
 	//Initialize Console
 	nuDebConWindowPos(0, 25, 25);
@@ -35,11 +30,8 @@ void stage00_update(void)
 {
 	nuContDataGetExAll(contData);
 	
-	x = 160 + contData[0].stick_x;
-	y = 120 - contData[0].stick_y;
-	
-	if ((contData[0].trigger & A_BUTTON))
-		SwitchColor();
+	pos_x = ICON_START_X_POS + contData[0].stick_x;
+	pos_y = ICON_START_Y_POS - contData[0].stick_y;
 	
 	// Set debug mode
 	if(contData[0].trigger & START_BUTTON){
@@ -47,15 +39,6 @@ void stage00_update(void)
 			debug = 0;
 		else
 			debug = 1;
-	}
-	
-	//Reset color to blue
-	if (contData[0].trigger & Z_TRIG)
-	{
-		r = 0;
-		g = 0;
-		b = 255;
-		color = 0;
 	}
 }
 
@@ -73,8 +56,9 @@ void stage00_draw(void)
 	if (debug) {
 		ClearBackground(0, 0, 0);
 	} else {
-		ClearBackground(r, g, b);
-		DrawLogo(x,y);
+		ClearBackground(DEF_BG_R, DEF_BG_G, DEF_BG_B);
+		DrawTitleImg(TITLE_X_POS,TITLE_Y_POS);
+		DrawLogo(pos_x,pos_y);
 	}
 
 	// Sync
@@ -87,12 +71,6 @@ void stage00_draw(void)
 	if (debug) {
 		nuDebConClear(NU_DEB_CON_WINDOW0);
 		nuDebConCPuts(NU_DEB_CON_WINDOW0, "Debug Menu\n\n");
-		sprintf(r_str, "%d", r);
-		nuDebConPrintf(NU_DEB_CON_WINDOW0, "R: %s\n", r_str);
-		sprintf(g_str, "%d", g);
-		nuDebConPrintf(NU_DEB_CON_WINDOW0, "G: %s\n", g_str);
-		sprintf(b_str, "%d", b);
-		nuDebConPrintf(NU_DEB_CON_WINDOW0, "B: %s\n", b_str);
 		nuDebConDisp(NU_SC_SWAPBUFFER);
 	}
 
@@ -117,6 +95,46 @@ void ClearBackground(u8 r, u8 g, u8 b)
     gDPPipeSync(glistp++);
 }
 
+void DrawTitleImg(int x, int y){
+	int w = 200, h = 66;
+	int i = 0, j = 0;
+	
+    gDPSetCycleType(glistp++, G_CYC_1CYCLE);
+    gDPSetCombineMode(glistp++, G_CC_DECALRGBA, G_CC_DECALRGBA);
+    gDPSetRenderMode(glistp++, G_RM_AA_ZB_TEX_EDGE, G_RM_AA_ZB_TEX_EDGE);
+    gDPSetDepthSource(glistp++, G_ZS_PRIM);
+    gDPSetPrimDepth(glistp++, 0, 0);
+    gDPSetTexturePersp(glistp++, G_TP_NONE);
+	
+	for (i=0; i<h; i++)
+	{
+		gDPLoadMultiTile(
+			glistp++,
+			IGMainWhite200,          // timg - Our sprite array
+			0,                        // tmem - Address to store in TMEM
+			G_TX_RENDERTILE,          // rt - Tile descriptor
+			G_IM_FMT_RGBA,            // fmt - Our image format
+			G_IM_SIZ_32b,             // size - Pixel size
+			w, h,                     // width, height of the full image
+			0, i,                     // Top left corner of the rectangle
+			w-1, i,                   // Bottom right corner
+			0,                        // Palette to use (always 0)
+			G_TX_WRAP, G_TX_WRAP,     // cms, cmt
+			G_TX_NOMASK, G_TX_NOMASK, // masks, maskt
+			G_TX_NOLOD, G_TX_NOLOD    // shifts, shiftt
+		);
+			
+		gSPTextureRectangle(glistp++, 
+			x << 2, y + i << 2, 
+			x + w << 2, y + i + 1 << 2,
+			G_TX_RENDERTILE, 
+			0 << 5, 0 << 5, 
+			1 << 10, 1 << 10);
+		
+		gDPPipeSync(glistp++);
+	}
+}
+
 void DrawLogo(int x, int y)
 {
     gDPSetCycleType(glistp++, G_CYC_1CYCLE);
@@ -139,39 +157,4 @@ void DrawLogo(int x, int y)
         0 << 5, 0 << 5, 
         1 << 10, 1 << 10);
     gDPPipeSync(glistp++);
-}
-
-void SwitchColor(){
-	switch(color){
-		case 0:
-			r = 0;
-			g = 255;
-			b = 0;
-			break;
-		case 1:
-			r = 255;
-			g = 0;
-			b = 0;
-			break;
-		case 2:
-			r = 255;
-			g = 255;
-			b = 255;
-			break;
-		case 3:
-			r = 0;
-			g = 0;
-			b = 255;
-			break;
-		default:
-			r = 0;
-			g = 0;
-			b = 0;
-			break;
-	}
-	
-	if(color > 2)
-		color = 0;
-	else
-		color += 1;
 }
